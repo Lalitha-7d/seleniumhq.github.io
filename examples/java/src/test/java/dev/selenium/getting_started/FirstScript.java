29eca8aa8d62b95837d82e253ffb39d6aa2564cd
Lalitha-7d <LALITTHADIVAKARUNI@gmail.com>
2025-06-08 19:40:37 +0530

    Update FirstScript.java

--- examples/java/src/test/java/dev/selenium/getting_started/FirstScript.java
+++ examples/java/src/test/java/dev/selenium/getting_started/FirstScript.java
@@ -10,22 +10,9 @@ import java.time.Duration;
 public class FirstScript {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-        driver.getTitle();
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        textBox.sendKeys("Selenium");
-        submitButton.click();
-
-        WebElement message = driver.findElement(By.id("message"));
-        message.getText();
-
-        driver.quit();
+driver.manage().window().maximize();
+        driver.get("https://github.com/login");
+thread.sleep(3000);
+       
     }
 }
